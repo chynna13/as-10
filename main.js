ad70977dd32b9209d3109ca6ac9f03ea346ded47
Chynna Williams <chynnawilliams@Chynnas-MacBook-Pro.local>
2018-11-05 14:03:57 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+	var slideIndex = 1; 
+	showSlides(slideIndex);
+
+	function showSlides(n){
+		var slides = $(".slides");
+		var dots = $(".dot");
+
+
+		if (n > slides.length){
+			slideIndex = 1; 
+		}
+		if (n < 1){
+			slideIndex = slides.length; 
+		}
+		for (var i=0; i < slides.length; i++){
+			slides[i].style.display="none";
+		}
+		for (var i = 0; i < dots.length; i++) {
+			dots[i].className = dots[i].className.replace("active","")
+		}
+
+		slides[slideIndex-1].style.display = "block";
+		dots[slideIndex-1].className += " active";
+	};
+
+	$(".next").click(function(){
+		slideIndex += 1; 
+		showSlides(slideIndex);
+	});
+
+	$(".dot").click(function(){
+		var index = $(".dot").index(this);
+		slideIndex = index + 1;
+		showSlides(slideIndex);
+	});
+
+	$(".before").click(function(){
+		slideIndex -= 1; 
+		showSlides(slideIndex);
+	});
+
+
+ 	$("img").fadeout();
+
+
+});
